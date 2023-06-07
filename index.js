2cf8151a27add9bbabe13ad81c58412247c95447
joseenrique.galicia.contractor <joseenrique.galicia.contractor@bbva.com>
2023-06-07 15:18:40 -0600

    changed standard to eslint and fix lints errors

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require("express")
+const express = require('express')
 
 const app = express()
 const PORT = process.env.PORT || 3977
 
-app.set("view engine", "ejs")
+app.set('view engine', 'ejs')
 
-app.get("/", (req, res) => {
-    res.render("index")
+app.get('/', (req, res) => {
+  res.render('index')
 })
 
 app.listen(PORT, () => {
-    console.log(`tu server esta listo en el puerto ${PORT}`)
+  console.log(`tu server esta listo en el puerto ${PORT}`)
 })
